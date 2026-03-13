868f4c5fb95461668b4be8b2ebcadebda535d151
TiredShyt <tiredofbeggingshyt@gmail.com>
2026-03-13 13:43:48 +0800

    chore: setup github actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' // Since I see this in your deps
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), 
-  ],
+  plugins: [react(), tailwindcss()],
+  base: '/portfolio/', // <--- Add this line!
 })
