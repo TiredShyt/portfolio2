ebbda1d5cc52dda39b83c79a28df646bd15efa1a
TiredShyt <tiredofbeggingshyt@gmail.com>
2026-03-13 13:44:08 +0800

    yeah

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite' // Since I see this in your deps
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/portfolio/', // <--- Add this line!
+  base: '/portfolio2/', // <--- Add this line!
 })
